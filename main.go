81a3269ef81dce19af1019e4724e2d18ac5907b8
Andres Villafañe <avillafane@telefe.com>
2020-08-13 01:31:53 -0300

    Se hace una lectura de la variable de entorno PORT si no está seteada se usa el valor default

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/bernnabe/mp/app"
 )
 
 func main() {
 	var configFilePath string
-	var serverPort string
+	var serverPort string = os.Getenv("PORT")
+
+	if serverPort == "" {
+		serverPort = "8080"
+	}
 
 	flag.StringVar(&configFilePath, "config", "config.yml", "absolute path to the configuration file")
-	flag.StringVar(&serverPort, "server_port", "8080", "port on which server runs")
+	flag.StringVar(&serverPort, "server_port", serverPort, "port on which server runs")
 	flag.Parse()
 
 	application := app.New(configFilePath)
